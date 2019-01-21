f8f89ebee63e34618f03603b298309230f12552a
ikantech <acer@yiim.net>
2019-01-21 23:44:51 +0800

    init android project

---
+++ yoga-android/app/src/main/java/net/yiim/yoga/yoga_android/MainActivity.java
@@ -0,0 +1,17 @@
+package net.yiim.yoga.yoga_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText("hello");
+    }
+}
